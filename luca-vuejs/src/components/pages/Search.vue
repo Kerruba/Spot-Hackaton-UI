a138b489ddf49ad166ffa8f550c67c4fdf810342
Kerruba <luca.cherubin@gmail.com>
2017-03-29 09:44:43 +0100

    Add a search box

--- luca-vuejs/src/components/pages/Search.vue
+++ luca-vuejs/src/components/pages/Search.vue
@@ -1,7 +1,20 @@
 <template>
-	<h2>Search Page</h2>	
+  <div>
+    <h2>Search OLS terms</h2>
+    <search-box></search-box>
+  </div>
 </template>
 
 <script>
-	
+  import Search from '@/components/parts/LocalSearchBox'
+  export default {
+    data () {
+      return {
+        'term': 'Hello world'
+      }
+    },
+    components: {
+      'search-box': Search
+    }
+  }
 </script>
